d61b753814bfba4671b12981669b63c180520b5a
Gaurav <cedcoss@cedcosss-MacBook-Pro-5.local>
2026-03-31 13:18:29 +0530

    Bump release marker for Polaris shell

--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_RELEASE = "2026-03-30-handle-home-fix-16";
-export const APP_COMMIT = "handle-home-fix-16";
+export const APP_RELEASE = "2026-03-31-polaris-shell-fix-17";
+export const APP_COMMIT = "polaris-shell-fix-17";
